8ee2204a82a91ac9262ec90e8084eed8743a1515
rakcurious <rakcurious@gmail.com>
2024-03-04 21:10:05 +0530

    Javascript1

---
+++ Day4_Javascript.js
@@ -0,0 +1,9 @@
+function Day_4_of_100DaysOfCode () {
+    return "Started Learning Javascript. Learnt About Variables, Functions, Execution Context, Call Stack, Hoisting, Lexical Environment, Scope and Scope Chain etc"
+
+        //Sources: Akshay Saini on YouTube, mdn web docs, Chai aur Code on YouTube.
+
+}
+
+console.log(Day_4_of_100DaysOfCode())
+
