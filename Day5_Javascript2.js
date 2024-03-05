8231ac25748c7f2fcbef380b54667cb44a04e0d6
rakcurious <rakcurious@gmail.com>
2024-03-05 23:19:14 +0530

    JavaScript 2

---
+++ Day5_Javascript2.js
@@ -0,0 +1,7 @@
+const Day5_of_100DaysOfCode = () => {
+    return "Learnt about Objects, Arrays, Higher Order Functions like map, reduce, filter etc, this keyword, date and time, JSON, IIFE, DOM Manipulation etc. "
+
+    //Sources: Chai aur Code on YouTube, mdn web docs
+}
+
+Day5_of_100DaysOfCode()
