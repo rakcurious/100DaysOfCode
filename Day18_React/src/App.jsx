367f6a6a40c138b1689b0c4a684231f247f7e639
rakcurious <rakcurious@gmail.com>
2024-03-18 17:03:34 +0530

    react bgchange project

--- Day18_React/src/App.jsx
+++ Day18_React/src/App.jsx
@@ -1,25 +1,44 @@
 import { useState } from "react";
 function App() {
+  const [color, setColor] = useState("purple");
 
-  let [counter, setCounter] = useState(0)
-
-          const minus = () => {
-            setCounter(counter-1)
-          }
-          const plus = () => {
-            setCounter(counter+1);
-          }
+  const changeColor = (col) => {
+    setColor(col);
+  };
   return (
     <>
-      <div className="flex justify-center items-center flex-col font-mono font-bold">
-      <h1 className="text-xl font-bold m-3">Counter</h1>
-      <div className="text-center p-2 flex justify-center items-center bg-slate-500 rounded-lg h-10 w-10" onClick={minus}><p className="text-xl">-</p></div>
-      <h1 className="text-xl m-2">Value: {counter}</h1>
-      <div className="text-center p-2 flex justify-center items-center bg-slate-500 rounded-lg h-10 w-10" onClick={plus}><p className="text-xl">+</p></div>
+      <div
+        className={`flex ${color} h-screen w-screen justify-center items-end  font-mono font-bold py-10`}
+      >
+        <div className="flex p-1 m-10 w-auto h-16 justify-center items-center font-mono font-bold bg-white/50  rounded-xl">
+          <div
+            className="text-center p-2 flex justify-center items-center bg-red-500 rounded-lg h-10 w-24 mx-4"
+            onClick={() => changeColor("bg-red-500")}
+          >
+            <p className="text-xl">Red</p>
+          </div>
+          <div
+            className="text-center p-2 flex justify-center items-center bg-green-500 rounded-lg h-10 w-24 mx-4"
+            onClick={() => changeColor("bg-green-500")}
+          >
+            <p className="text-xl">Green</p>
+          </div>
+          <div
+            className="text-center p-2 flex justify-center items-center bg-blue-500 rounded-lg h-10 w-24 mx-4"
+            onClick={() => changeColor("bg-blue-500")}
+          >
+            <p className="text-xl">Blue</p>
+          </div>
+          <div
+            className="text-center p-2 flex justify-center items-center bg-cyan-500 rounded-lg h-10 w-24 mx-4"
+            onClick={() => changeColor("bg-cyan-500")}
+          >
+            <p className="text-xl">Cyan</p>
+          </div>
+        </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
