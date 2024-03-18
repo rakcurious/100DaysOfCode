1ea422f192d171374a457930cb5900ca79123071
rakcurious <rakcurious@gmail.com>
2024-03-18 13:49:53 +0530

    react counter

--- Day18_React/src/App.jsx
+++ Day18_React/src/App.jsx
@@ -1,9 +1,23 @@
-
+import { useState } from "react";
 function App() {
 
+  let [counter, setCounter] = useState(0)
+
+          const minus = () => {
+            setCounter(counter-1)
+          }
+          const plus = () => {
+            setCounter(counter+1);
+          }
   return (
     <>
       <h1>yo mr white</h1>
+
+      <h1>Counter</h1>
+      <br />
+      <button onClick={minus}>-</button>
+      <h1>Value: {counter}</h1>
+      <button onClick={plus}>+</button>
     </>
   )
 }
