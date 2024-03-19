38e79ce3dea22b70207140644709daca451bfe52
rakcurious <rakcurious@gmail.com>
2024-03-19 09:36:19 +0530

    React Router Project

---
+++ Day19_React2/React_Router_Project/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-light font-crypto  text-5xl text-violet-300 text-center m-4">Gm Crypto Bros
+</h1>
+    </>
+  )
+}
+
+export default App
