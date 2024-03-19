70cb02b78a1b22967b5cf86c5a0a10a38dda675e
rakcurious <rakcurious@gmail.com>
2024-03-19 15:44:48 +0530

    React CurrencyConverter Project

--- Day19_React2/React_Router_Project/src/components/CurrencyConverter.jsx
+++ Day19_React2/React_Router_Project/src/components/CurrencyConverter.jsx
@@ -2,26 +2,29 @@ import React, {useId} from 'react'
 
 function CurrencyConverter({
     
-    // label,
-    // amount,
-    // onAmountChange,
-    // onCurrencyChange,
-    // currencyOptions = [],
-    // selectCurrency = "usd",
-    // amountDisable = false,
-    // currencyDisable = false,
-    // className = "",
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
 }) {
    const amountInputId = useId()
 
     return (
-        <div className={`bg-gradient-to-r from-cyan-800 to-purple-800 p-3 rounded-lg text-lg font-bold flex`}>
+        <div className={`bg-gradient-to-r from-cyan-800 to-purple-800 p-3 rounded-lg text-lg font-bold flex ${className}`}>
             <div className="w-1/2 flex flex-col">
-                <label   className="text-black/40 mb-2 inline-block">
-                    from
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
+                    {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    disabled={amountDisable}
                     className="outline-none rounded-md w-1/2 bg-white px-2 py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -31,14 +34,21 @@ function CurrencyConverter({
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency</p>
                 <select
-                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    className="rounded-lg w-1/2 px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                     
                 >
-                    
+                    {
+                        currencyOptions.map((currency) => (
+                           <option key={currency} value={currency}>
+                            {currency}
+                            </option> 
+                        ))
+                    }
                         
-                            <option >
-                            USD
-                            </option>
+                            
                 
                 </select>
             </div>
